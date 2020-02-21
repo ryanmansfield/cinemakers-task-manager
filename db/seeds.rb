d300498c7a12fa6c8c0e3f725f9278874761be3b
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-02-20 19:04:14 -0600

    Added to Users Attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "ryan", last_name: "mansfield", email: "ryan@test.com", password: "123456" )
+User.create(first_name: "joe", last_name: "camel", email: "joet@test.com", password: "123456" )
+User.create(first_name: "jill", last_name: "hill", email: "jill@test.com", password: "123456" )
+User.create(first_name: "jack", last_name: "beanstalk", email: "jack@test.com", password: "123456" )
+
+puts "Users Created"
