df295df1872c04f7bc84253fe1613260d4f8cd92
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-02-21 13:56:25 -0600

    Project Model Associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
   belongs_to :team
-  has_many :users, through: :team
+  has_many :collaborators, through: :team
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -4,4 +4,5 @@ class Team < ApplicationRecord
 
   has_many :projects
   validates :name, presence: true, allow_blank: false
+  validates :team, presence: true, allow_blank: false
 end
