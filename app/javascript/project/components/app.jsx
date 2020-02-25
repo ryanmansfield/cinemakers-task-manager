1dbded5b4fe2371554fc7889f4dc44642c4b3c25
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-02-24 22:20:45 -0600

    Stages React App structure implemented

---
+++ app/javascript/project/components/app.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ChannelList from '../containers/channel_list';
+import MessageList from '../containers/message_list';
+
+
+
+const App = (props) => {
+  return (
+    <div className="messaging-wrapper">
+      <div className="logo-container">
+       {/** <img className="messaging-logo" src="../../../app/assets/images/logo.png" alt="logo" />*/}
+      </div>
+        <ChannelList selectedChannel={props.match.params.channel} />
+    </div>
+  );
+};
+
+export default App;
+
+
+// import React from 'react';
+// import ChannelList from '../containers/channel_list';
+// import MessageList from '../containers/message_list';
+
+
+
+// const App = (props) => {
+//   return (
+//     <div className="messaging-wrapper">
+//       <div className="logo-container">
+//        {* <img className="messaging-logo" src="../../../app/assets/images/logo.png" alt="logo" />}
+//       </div>
+//         <ChannelList selectedChannel={props.match.params.channel} />
+//         <MessageList selectedChannel={props.match.params.channel} />
+//     </div>
+//   );
+// };
+
+// export default App;
