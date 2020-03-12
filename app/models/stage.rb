dfae7056977f74902dd9e68961162d346242394e
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-03-12 05:30:14 -0500

    progress method added to model

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -8,7 +8,7 @@ class Stage < ApplicationRecord
     task_count = self.tasks.count
     completed_tasks = 0.0
     tasks.each { |task| completed_tasks +=1 if task.is_complete == true  }
-    progress = (completed_tasks / task_count)*100
-    return progress.to_i
+    progress = ((completed_tasks / task_count)*100).to_i
+    return progress
   end
 end
