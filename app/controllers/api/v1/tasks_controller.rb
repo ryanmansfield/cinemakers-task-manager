04709263b92357457ade6b8cbef57a11001d195c
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-03-13 13:32:19 -0500

    redux state fixed for destroy actions

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -18,20 +18,20 @@ class Api::V1::TasksController < ApplicationController
 
 
   def update
-    @task = Task.find(params[:id])
-    @task.update(title: task_params[:title],
+    task = Task.find(params[:id])
+    task.update(title: task_params[:title],
                  note: task_params[:note],
                  due_date: task_params[:due_date],
                  assigned_to: task_params[:assigned_to],
                  is_complete: task_params[:is_complete]
                 )
-    head 204
+    render json: task
   end
 
   def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-    head 204
+    task = Task.find(params[:id])
+    task.destroy
+    render json: task
   end
 
   private
