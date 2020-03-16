bcd7862bef226d8201af789b2f9aaa7bdac992b2
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-03-15 19:11:16 -0500

    project_progress method fixed

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,11 +7,15 @@ class Project < ApplicationRecord
   has_many :tasks, through: :checklists
 
   def project_progress
-    tasks = self.tasks
-    task_count = self.tasks.count
-    completed_tasks = 0.0
-    tasks.each { |task| completed_tasks +=1 if task.is_complete == true  }
-    progress = ((completed_tasks / task_count)*100).to_i
-    return progress
+    if self.tasks.exists?
+      tasks = self.tasks
+      task_count = self.tasks.count
+      completed_tasks = 0.0
+      tasks.each { |task| completed_tasks +=1 if task.is_complete == true  }
+      progress = ((completed_tasks / task_count)*100).to_i
+      return progress
+    else
+      return 0
+    end
   end
 end
