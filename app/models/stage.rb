bae30c4891a781faf2780dd4c25d3d8d7b67ad12
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-03-15 23:20:00 -0500

    style added to home page

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -4,11 +4,15 @@ class Stage < ApplicationRecord
   has_many :tasks, through: :checklists
 
   def stage_progress
-    tasks = self.tasks
-    task_count = self.tasks.count
-    completed_tasks = 0.0
-    tasks.each { |task| completed_tasks +=1 if task.is_complete == true  }
-    progress = ((completed_tasks / task_count)*100).to_i
-    return progress
+    if self.tasks.exists?
+      tasks = self.tasks
+      task_count = self.tasks.count
+      completed_tasks = 0.0
+      tasks.each { |task| completed_tasks +=1 if task.is_complete == true  }
+      progress = ((completed_tasks / task_count)*100).to_i
+      return progress
+    else
+      return 0
+    end
   end
 end
