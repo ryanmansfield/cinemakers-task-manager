05182b001336f0d6eaf60d99540b1c5630987b39
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-04-22 18:03:34 -0500

    search2 gem added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,14 @@
+//= require jquery
 //= require rails-ujs
 //= require_tree .
+//= require select2
+
+$(document).ready(function(){
+  // Turn on js-selectable class so that it becomes SELCT 2 tag
+  $('.js-searchable').select2({
+    placeholder: "Choose a person",
+    allowClear: true,
+    width: 400,
+    theme: "bootstrap"
+  });
+});
