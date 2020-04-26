85a2261aa0eddf644e146f4b2deb4b93c8906e31
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-04-25 22:34:21 -0500

    datepicker css

--- app/javascript/project/index.jsx
+++ app/javascript/project/index.jsx
@@ -6,7 +6,7 @@ import logger from 'redux-logger'
 import ReduxPromise from 'redux-promise';
 // import Moment from 'react-moment';
 // import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import "react-datepicker/dist/react-datepicker.css";
+// import "react-datepicker/dist/react-datepicker.css";
 
 import App from './components/app';
 import checklistsReducer from './reducers/checklists_reducer';
