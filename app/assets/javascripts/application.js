7fed2c4e1be3e83d2986e0cc937611b0a7f81b04
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-05-01 16:21:31 -0500

    collaborator modal added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,49 @@ $(document).ready(function(){
   // Turn on js-selectable class so that it becomes SELCT 2 tag
   $('.js-searchable').select2({
     theme: "bootstrap",
-    multiple: true,
+    multiple: false,
     allowClear: true,
     minimumInputLength: 3,
-    placeholder: "Search User",
+    placeholder: "Search By Email",
+    width: '400px',
+    dropdownAutoWidth: 'true',
+    // closeOnSelect: false,
+
 
   });
 });
+
+// $('#id').select2('focus');
+
+
+
+// document.getElementById("MyElement").classList.add('MyClass');
+
+// document.getElementById("MyElement").classList.remove('MyClass');
+
+// if ( document.getElementById("MyElement").classList.contains('MyClass') )
+
+// // document.getElementById("MyElement").classList.toggle('MyClass');
+// $(document).on('focus', '.select2-selection.select2-selection--single', function (e) {
+//   $(this).closest(".select2-container").siblings('select:enabled').select2('open');
+// });
+
+// // steal focus during close - only capture once and stop propogation
+// $('select.select2').on('select2:closing', function (e) {
+//   $(e.target).data("select2").$selection.one('focus focusin', function (e) {
+//     e.stopPropagation();
+//   });
+// });
+
+
+
+// $('#id').select2('open'); will set focus and open the select2 ready for search.
+
+// var list = $("#fancy").select2({
+//   closeOnSelect: false
+// }).on("select2:closing", function(e) {
+//   e.preventDefault();
+// }).on("select2:closed", function(e) {
+//   list.select2("open");
+// });
+// list.select2("open");
